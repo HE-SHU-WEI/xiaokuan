72b674fa347802e95fcdeb34a27720a34217110c
HE-SHU-WEI <william900508@gmail.com>
2023-12-10 16:46:07 +0800

    231210

---
+++ aaa.php
@@ -0,0 +1,1 @@
+<img src="C:\xampp\htdocs\xiaokuan\storage\app\photos\6YLLk9MQZRaPrRdMfEsSVXgAAgHjAUPB19oQh01t.jpg">
--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -15,34 +15,34 @@
         @csrf
 
         <label for="teacher_name">Teacher Name:</label>
-        <input type="text" name="teacher_name" required>
+        <input type="text" name="teacher_name" required><br>
 
         <label for="classname">Class Name:</label>
-        <input type="text" name="classname" required>
+        <input type="text" name="classname" required><br>
 
         <label for="youtube_link">YouTube Link:</label>
-        <input type="url" name="youtube_link" required>
+        <input type="url" name="youtube_link" required><br>
 
         <label for="youtube_id">YouTube Video ID:</label>
-        <input type="text" name="youtube_id" required>
+        <input type="text" name="youtube_id" required><br>
 
         <label for="introduction">Introduction:</label>
-        <textarea name="introduction" required></textarea>
+        <textarea name="introduction" required></textarea><br>
 
         <label for="know">Know:</label>
-        <textarea name="know" required></textarea>
+        <textarea name="know" required></textarea><br>
 
         <label for="for_who">For Who:</label>
-        <textarea name="for_who" required></textarea>
+        <textarea name="for_who" required></textarea><br>
 
         <label for="photo">Photo:</label>
-        <input type="file" name="photo" accept="image/*" required>
+        <input type="file" name="photo" accept="image/*" required><br>
 
         <label for="money">Money:</label>
-        <input type="number" name="money" required>
+        <input type="number" name="money" required><br>
 
         <label for="classtype">課程種類：</label>
-        <input type="text" name="classtype" id="classtype" required>
+        <input type="text" name="classtype" id="classtype" required><br>
 
         <button type="submit">Upload Teacher Info</button>
     </form>
